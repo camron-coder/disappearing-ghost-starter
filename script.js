57d3a76ebd0be4796f6ae42e81c31923a730bfea
Glitch (windy-bittersweet-mailbox) <none>
2020-11-18 00:14:40 +0000

    💆☺️ Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+$(".hide-button").click(function(){
+  $(".ghost-image").hide();
+});
 
+
+$(".show-button").click(function(){
+  $(".ghost-image").show();
+});
+
+$(".update-image-button").click(function(){
+  
+});
